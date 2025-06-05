31e3eb6eb5828ceb671e88d7da364f1847d97d6b
sandra <karwowskasandra@gmail.com>
2025-06-05 10:15:31 +0200

    Segundo commit

---
+++ pecera.java
@@ -0,0 +1,21 @@
+public double getCapacidadLitros() {
+	return capacidadLitros;
+}
+
+public String get gettipoAgua() {
+	return tipoAgua;
+}
+
+public String getPezHabitante() {
+	return pezHabitante;
+}
+
+public void setPezHabitante(String pezHabitante) {
+    if (pezHabitante != null) {
+	pezHabitante = pezHabitante;
+	System.out.println(pezHabitante + "Ha sido asignado a la pecera");
+    } else {
+	pezHabitante = null;
+	System.out.println("El habitante ha sido eliminado de la pecera";
+    }
+}
