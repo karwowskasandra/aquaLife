44d9dd87a32ca54d31cbf2d1dc1d7b90556205be
sandra <karwowskasandra@gmail.com>
2025-06-05 12:39:42 +0200

    Sexto commit

--- pez.java
+++ pez.java
@@ -6,5 +6,13 @@ public class pez {
 public pez(String nombre, int salud) {
     nombre = nombre;
     salud = salud;
-}
+ }
 
+public String getNombre() {
+    return nombre;
+ }
+
+public String getEstado() {
+    return "nombre: " + nombre + "salud : " + salud;
+ }
+}
