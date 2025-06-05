46dbad5f97ef9954c566f9bb66b8d43036500eb3
juanjferote <juanj.ferote@educa.jcyl.es>
2025-06-05 09:37:39 +0200

    primer commit

---
+++ Pecera.java
@@ -0,0 +1,12 @@
+public class Pecera {
+    private double capacidadLitros;
+    private String tipoAgua;
+    private String pezHabitante;
+ 
+   
+    public Pecera(double capacidadLitros, String tipoAgua) {
+        capacidadLitros = capacidadLitros;
+        tipoAgua = tipoAgua;
+        pezHabitante = null;
+    }
+}
---
+++ Pez.java
@@ -0,0 +1,16 @@
+public class Pez
+{
+    // instance variables - replace the example below with your own
+    private String nombre;
+    private int salud;
+
+    /**
+     * Constructor for objects of class Pez
+     */
+    public Pez(String nombre, int salud)
+    {
+        // initialise instance variables
+        nombre = nombre;
+        salud = salud;
+    }
+}
